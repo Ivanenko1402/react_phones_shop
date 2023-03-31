ce209e386f3b89201b2656248930f3dc82ee6e38
Ivanenko1402 <Ivanenko1402@gmail.com>
2023-03-31 20:00:22 +0300

    add swipe on product carousel

--- src/components/ProductsSlider/ProductsSlider.tsx
+++ src/components/ProductsSlider/ProductsSlider.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
+// import { useSwipeable } from 'react-swipeable';
 
 import { Phone } from '../../types/Phone';
 import { ProductCard } from '../ProductCard';
@@ -94,6 +95,58 @@ export const ProductsSlider: React.FC<Props> = ({
     })
   };
 
+  const swipe = (event: React.TouchEvent<HTMLDivElement>) => {
+    const startTouchX = event.touches[0].clientX;
+    const touchMoveHandler = (event: TouchEvent) => {
+      const currentPosition = event.touches[0].clientX;
+      const diff = startTouchX - currentPosition;
+
+      if (diff > 0) {
+        const itemWidth = 270;
+        const gap = 15;
+        const maxPosition = -((phoneList.length) * itemWidth + (phoneList.length - 1) * gap) + width;
+        const minPosition = 0;
+        let step = 10;
+    
+        setPosition(prev => {
+          if (prev === maxPosition) {
+            return minPosition;
+          }
+    
+          if (prev - step < maxPosition) {
+            return maxPosition;
+          }
+    
+          return prev - step;
+        })
+      } else if (diff < 0) {
+        const itemWidth = 270;
+        const gap = 15;
+        const maxPosition = -((phoneList.length) * itemWidth + (phoneList.length - 1) * gap) + width;
+        const minPosition = 0;
+        let step = 10;
+    
+        setPosition(prev => {
+          if (prev === minPosition) {
+            return maxPosition;
+          }
+    
+          if (prev + step > minPosition) {
+            return minPosition;
+          }
+    
+          return prev + step;
+        })
+      }
+    }
+    const touchEndHandler = () => {
+      document.removeEventListener('touchmove', touchMoveHandler);
+      document.removeEventListener('touchend', touchEndHandler);
+    }
+    document.addEventListener('touchmove', touchMoveHandler);
+    document.addEventListener('touchend', touchEndHandler);
+  }
+
   return (
     <div ref={container} className="productSlider">
       <div className="productSlider_top">
@@ -119,11 +172,12 @@ export const ProductsSlider: React.FC<Props> = ({
           </button>
         </div>
       </div>
-
       <div
+        // {...swipeHandlers}
         style={{ 'left': `-${position}` }}
         className='productSlider_conteiner_content'
-      >
+        onTouchMove={(event) => swipe(event)}
+        >
         {phoneList.map(phone => (
           <ProductCard
             phone={phone}
